35168cf802800b92a45fe1cf79d6c4ff53b10c1b
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-03-09 00:24:00 +0100

    Add files via upload

---
+++ compiler.js
@@ -0,0 +1,69 @@
+const { log } = require("console")
+const fs = require("fs")
+const path = require("path")
+var global = ""
+
+const inpPath = "index.code"
+const outPath = "C:\\Users\\Manuel Westermeier\\source\\repos\\cpp-server-script\\cpp-server-script.cpp"
+
+const parsed = parseFile(inpPath)
+
+fs.writeFileSync(outPath,
+    `${fs.readFileSync("std.cpp", "utf-8")}
+${global}
+int main(int argc, char* argv[]) {
+    //user from code
+    ${parsed}
+    //usercode end
+}`
+    , "utf-8")
+
+function parseFile(path = "") {
+
+    const inp = fs.readFileSync(path, "utf-8").replace(/\r/, "")
+    var out = ""
+
+    out += inp.split("\n").map((line, i) => {
+
+        const tokens = line.split(/\s/).filter(t => t != "")
+        var lineOut = ""
+
+        if (tokens[0] == "fn") {
+
+            const name = tokens[1]
+            const args = []
+
+            if (tokens.length - 4 > 0) {
+
+                const unparsedArgs = tokens.slice(2, tokens.length - 2)
+
+                for (let index = 0; index < unparsedArgs.length; index++) {
+
+                    const [name, type] = unparsedArgs[index].split("#")
+
+                    if (!name || !type) {
+                        log(`wrong argument syntax : ${path}:${i} -> '${line}'`)
+                    }
+
+                    args.push(`${type} ${name}`)
+
+                }
+
+            }
+
+            lineOut = `auto ${name} = [&](${args.join(",")}) {`;
+
+        }
+        else if (tokens[0] == "#include") {
+            global += line
+            return ""
+        }
+        else return line
+
+        return lineOut
+
+    }).join("\n")
+
+    return out;
+
+}
---
+++ std.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define sf to_string
+
+void out(string data)
+{
+    cout << data << endl;
+}
+
+// Manuel Westermeier
